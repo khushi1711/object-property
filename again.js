a1c40dcae0841ab4dcdf17bef0ecdbd4b0c24f30
khushi1711 <90022273+khushi1711@users.noreply.github.com>
2021-11-26 17:27:28 +0530

    Add files via upload

---
+++ again.js
@@ -0,0 +1,26 @@
+ var person={
+     p1:{
+         FirstName:"Khushi",
+         LastName:"kumari",
+         OfficeAddress:"H67 gaya",
+     },
+     p2:{
+        FirstName:"Anjali",
+        LastName:"kumari",
+        OfficeAddress:"patna b7",
+     },
+     p3:{
+        FirstName:"Riya",
+        LastName:"Singh",
+        OfficeAddress:"bhuj Gujarat",
+     }};
+     console.log(person.p1.FirstName);
+     console.log(person.p1.LastName);
+     console.log(person.p1.OfficeAddress);
+     console.log(person.p2.FirstName);
+     console.log(person.p2.LastName);
+     console.log(person.p2.OfficeAddress);
+     console.log(person.p3.FirstName);
+     console.log(person.p3.LastName);
+     console.log(person.p3.OfficeAddress);
+     
